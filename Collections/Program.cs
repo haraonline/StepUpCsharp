0f28a712465c3e9f253b3367498eb44ef20e240d
Hara Gopal Kovvali <haragopal.kovvali@gmail.com>
2023-04-02 14:15:57 +0200

    some more

---
+++ Collections/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+
+namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var e = Enumerable.Range(1, 10);
+            foreach (var i in e)
+            {
+                System.Console.WriteLine(i);
+            }
+
+        }
+    }
+}
